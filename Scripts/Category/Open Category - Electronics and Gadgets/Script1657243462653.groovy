6e2e67bc9313749b13d211b61186ac0db6fd83c5
Gusti Fahmi Fadhila <gustifahmifadhila@gmail.com>
2022-07-08 08:35:41 +0700

    add Open Category Test Cases (On Progress)

---
+++ Scripts/Category/Open Category - Electronics and Gadgets/Script1657243462653.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\095059\\Documents\\BTDP-Katalon\\Assignment3\\apk\\ECommerce -SAMPLE-Android.apk', true)
+
+Mobile.waitForElementPresent(findTestObject('Recent/Button - Category'), 0)
+
+Mobile.tap(findTestObject('Recent/Button - Category'), 0)
+
+Mobile.waitForElementPresent(findTestObject('Category/Text - Category'), 0)
+
+Mobile.tap(findTestObject('Category/Category - Electronics and Gadgets'), 0)
+
+Mobile.verifyElementVisible(findTestObject('Category/Electronics and Gadgets/Text - Electronics and Gadgets'), 0)
+
+Mobile.closeApplication()
+
