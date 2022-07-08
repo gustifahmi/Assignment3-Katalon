2619e4e33cd0e6098c2daf57f2b13fb1b22d779a
Gusti Fahmi Fadhila <gustifahmifadhila@gmail.com>
2022-07-08 09:49:25 +0700

    completing Test Case Open Category - Home and Furniture

--- Scripts/Category/Open Category - Home and Furniture/Script1657243982371.groovy
+++ Scripts/Category/Open Category - Home and Furniture/Script1657243982371.groovy
@@ -39,5 +39,27 @@ Mobile.verifyElementVisible(findTestObject('Category/Home and Furniture/Image - 
 
 Mobile.verifyElementVisible(findTestObject('Category/Home and Furniture/Image - Item 5'), 0)
 
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 1 Name'), 'Bathroom Toilet Wall Shelves A289')
+
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 1 Price'), '8 USD')
+
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 2 Name'), 'Copper Light 3 Battery Warm White - Fairy Light Battery ')
+
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 2 Price'), '9 USD')
+
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 3 Name'), 'Bed Mosquito Net KL200 | 200 x 200 cm')
+
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 3 Price'), '19 USD')
+
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 4 Name'), 'Robot Vacuum Cleaner Wellcomm Home 2.0')
+
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 4 Price'), '250 USD')
+
+Mobile.scrollToText('Multifunction Stand Hanger', FailureHandling.STOP_ON_FAILURE)
+
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 5 Name'), 'Multifunction Stand Hanger')
+
+Mobile.verifyElementText(findTestObject('Category/Home and Furniture/Text - Item 5 Price'), '12 USD')
+
 Mobile.closeApplication()
 
